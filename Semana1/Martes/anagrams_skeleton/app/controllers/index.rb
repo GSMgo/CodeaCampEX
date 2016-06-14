a419f1822d7aa106b061a15105f267fe07333e41
codeamac02 <codeamac02@gmail.com>
2016-06-14 18:14:25 -0500

    ..

--- Semana1/Martes/anagrams_skeleton/app/controllers/index.rb
+++ Semana1/Martes/anagrams_skeleton/app/controllers/index.rb
@@ -3,11 +3,11 @@ get '/' do
 end
 
 get '/:new_word' do
-  @new_word = params[:new_word] #obtiene palabra(parametro)
+  @new_word = params[:new_word] #obtiene palabra(desde el parametro)
   @anagrams = Word.get_anagrams(@new_word) #va al modelo y corre el método y guarda el arreglo de resultados
   erb :word #corre la vista
 end
 
 post '/new_word' do
-  redirect to ("/#{params[:user_word]}")
+  redirect to ("/#{params[:user_word]}") # obtiene la palabra desde el input de texto
 end
