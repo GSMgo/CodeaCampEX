b5b75219bfd3b8a7e2264630c2d90d4beac4fa57
codeamac02 <codeamac02@gmail.com>
2016-06-17 12:58:39 -0500

    users finished 1/2

--- Semana1/Miercoles/UserAdmin/app/models/user.rb
+++ Semana1/Miercoles/UserAdmin/app/models/user.rb
@@ -4,9 +4,14 @@ class User < ActiveRecord::Base
    # ej. User.authenticate('fernando@codea.mx', 'qwerty')
 
   def self.authenticate(email, password)
-
     # si el email y el password corresponden a un usuario valido, regresa el usuario
     # de otra manera regresa nil
+    is_user = nil
+    User.all.each do|x|
+      if x.email == email && x.password == password
+        return x
+      end
+    end
   end
 
 end
