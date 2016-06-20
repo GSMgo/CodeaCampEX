459d5b666180b04afb08e9720bffaef04d417dfb
codeamac02 <codeamac02@gmail.com>
2016-06-20 17:32:50 -0500

    slot machine

--- Semana1/Jueves/slot_machine/public/js/application.js
+++ Semana1/Jueves/slot_machine/public/js/application.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+
+  $(function(){
+    $('form').on('submit',function(event){
+        event.preventDefault() ;
+        alert("Form Submission stoped.");
+    });
+  });
   
 });
